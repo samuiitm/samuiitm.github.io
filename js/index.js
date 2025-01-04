5bf2f629496e9af628c0d7569c321aa985113cc6
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2025-01-04 22:52:29 +0100

    Implement multi-language support: Added English and Spanish translations to the site.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,115 @@
+// Translations in english and spanish
+const translations = {
+    en: {
+        title: 'Samuel Cañadas - Aspiring Developer',
+        about: 'About',
+        projects: 'Projects',
+        experience: 'Experience',
+        contact: 'Contact',
+        aspiring_dev: 'Aspiring Developer',
+        about_large_first_paragraph: `There’s nothing more exciting than watching my code take shape and turn ideas into reality. I’m always seeking opportunities to grow, and every challenge fuels my passion to keep pushing forward.`,
+        about_large_second_paragraph: `When I’m not coding, I explore all types of games, especially story-driven ones, hit the gym, and let music inspire my creativity—keeping me disciplined and motivated for every project.`,
+        technologies: 'Technologies',
+        kobe_quote: `"Dedication makes dreams come true."`,
+        kobe_name: `Kobe Bryant`,
+        joel_quote: `"No matter what, you keep finding something to fight for."`,
+        joel_name: `Joel Miller`,
+        dream_title: `From player to creator`,
+        dream_text: `I grew up playing video games, but Naughty Dog’s masterpieces sparked my love for story-driven experiences. My dream is to one day join their team and create games that inspire and immerse players as their work has inspired me.`,
+        projects_section_subtitle: `Take a look to the projects I have made along the time as a student and internship`,
+        project_1_description: `An horitzontal shoot 'em up videogame where you control a spaceship tasked with destroying alien ships escaping from Area 51. This is my first game developed in Unity and programmed in C#.`,
+        project_2_title: `HANGMAN`,
+        project_2_description: `Hangman is a word-guessing game where players try to guess a hidden word by suggesting letters. Each incorrect guess adds a part to a hanging figure, and the game ends if the figure is fully drawn`,
+        project_3_title: `FINAL PROJECT SMX`,
+        project_3_description: `I designed the IT infrastructure for a fictional company, covering network setup, DHCP, DNS, web servers (WordPress, Moodle), security with Squid proxy and pfSense, and documentation.`,
+        experience_section_subtitle: `A professional journey in progress: learning and evolving.`,
+        first_experience_role: `IT Support Intern`,
+        first_experience_company: `Rocagrossa Institute`,
+        first_experience_location: `Lloret de Mar, Spain`,
+        first_experience_date: `Oct 2023 - Feb 2024`,
+        first_experience_task_1: `Resolved technical issues in classrooms, including both hardware and software problems.`,
+        first_experience_task_2: `Provided software troubleshooting and support for Chromebooks used by students.`,
+        first_experience_task_3: `Created a detailed inventory of the institute’s IT systems.`,
+        first_experience_task_4: `Deployed new operating system images on desktop computers, updating outdated systems and configuring them to prevent unwanted changes using tools like Deep Freeze.`,
+        first_experience_task_5: `Performed routine hardware maintenance, including cleaning components and repairing computers to avoid premature disposal.`,
+        second_experience_role: `Digital Marketing Intern`,
+        second_experience_company: `PAX Jovem`,
+        second_experience_location: `Beja, Portugal`,
+        second_experience_date: `Apr 2024 - May 2024`,
+        second_experience_task_1: `Designed promotional materials such as posters, flyers, and social media posts.`,
+        second_experience_task_2: `Assisted in creating digital content to enhance the visibility of the "Futebol de Rua" project across social media platforms.`,
+        second_experience_task_3: `Supported the promotion of sports and cultural activities organized by the project, contributing to raising awareness about social inclusion through sports.`,
+        second_experience_task_4: `I dedicated time to assist team members with a simple, in-person user guide, helping them become familiar with digital content creation tools such as Canva, ensuring they could confidently utilize these resources to enhance their productivity and creativity.`,
+        contact_section_title: `MAKE IT POSSIBLE, THE 'IM' HAS NO PLACE IF YOU DECIDE IT.`,
+        contact_section_subtitle: `I’m working toward my own dream, and I’d love to partner with others who dream big. Drop me a message on LinkedIn or email!`,
+    },
+    es: {
+        title: 'Samuel Cañadas - Aspirante Desarrollador',
+        about: 'Sobre mí',
+        projects: 'Proyectos',
+        experience: 'Experiencia',
+        contact: 'Contacto',
+        aspiring_dev: 'Aspirante Desarrollador',
+        about_large_first_paragraph: `No hay nada más emocionante que ver mi código cobrar vida y convertir ideas en realidad. Siempre busco oportunidades de crecimiento, y cada desafío me impulsa a seguir avanzando.`,
+        about_large_second_paragraph: `Fuera del código, disfruto de los videojuegos, especialmente los narrativos, voy al gimnasio, y la música me inspira, manteniéndome disciplinado y motivado en cada proyecto.`,
+        technologies: 'Tecnologías',
+        kobe_quote: `"La dedicación hace que los sueños se hagan realidad."`,
+        kobe_name: `Kobe Bryant`,
+        joel_quote: `"No importa lo que pase, sigues encontrando algo por lo que luchar."`,
+        joel_name: `Joel Miller`,
+        dream_title: `De jugador a creador`,
+        dream_text: `Crecí jugando videojuegos, pero los maestros de Naughty Dog encendieron mi amor por las experiencias narrativas. Mi sueño es unirme a su equipo algún día y crear juegos que inspiren e inmersen a los jugadores como ellos me han inspirado a mí.`,
+        projects_section_subtitle: `Echa un vistazo a los proyectos que he desarrollado a lo largo del tiempo como estudiante e interno.`,
+        project_1_description: `Un videojuego de disparos horizontales en el que controlas una nave espacial destinada a destruir naves alienígenas que escapan del Área 51. Este es mi primer juego desarrollado en Unity y programado en C#.`,
+        project_2_title: `AHORCADO`,
+        project_2_description: `El Ahorcado es un juego de adivinanza donde los jugadores deben adivinar una palabra oculta con letras. Cada fallo agrega una parte a una figura colgante, y el juego termina si la figura completa se dibuja.`,
+        project_3_title: `PROYECTO FINAL SMR`,
+        project_3_description: `Diseñé la infraestructura IT para una empresa ficticia, realizando la configuración de redes, DHCP, DNS, servidores web (WordPress, Moodle), seguridad con proxy Squid y pfSense, documentando detalladamente el todo.`,
+        experience_section_subtitle: `Una carrera profesional en progreso: aprendiendo y evolucionando.`,
+        first_experience_role: `Interno de Soporte Técnico`,
+        first_experience_company: `IES Rocagrossa`,
+        first_experience_location: `Lloret de Mar, España`,
+        first_experience_date: `Oct 2023 - Feb 2024`,
+        first_experience_task_1: `Resolví problemas técnicos en aulas, tanto en el ámbito del hardware como en el del software.`,
+        first_experience_task_2: `Ofrecí soporte y resolución de problemas de software para Chromebooks utilizados por los estudiantes.`,
+        first_experience_task_3: `Creé un inventario detallado de los sistemas IT del instituto.`,
+        first_experience_task_4: `Implementé imágenes de sistema operativo en computadoras de escritorio, actualizando sistemas desactualizados y configurando para prevenir cambios no deseados con herramientas como Deep Freeze.`,
+        first_experience_task_5: `Realicé mantenimiento rutinario del hardware, incluyendo limpieza de componentes y reparación de ordenadores para evitar su descarte prematuro.`,
+        second_experience_role: `Interno de Marketing Digital`,
+        second_experience_company: `PAX Jovem`,
+        second_experience_location: `Beja, Portugal`,
+        second_experience_date: `Abr 2024 - May 2024`,
+        second_experience_task_1: `Diseñé materiales promocionales como carteles, folletos y publicaciones en redes sociales.`,
+        second_experience_task_2: `Ayudé a crear contenido digital para mejorar la visibilidad del proyecto "Futebol de Rua" en las plataformas de redes sociales.`,
+        second_experience_task_3: `Apoyé la promoción de actividades deportivas y culturales organizadas por el proyecto, contribuyendo a crear conciencia sobre la inclusión social a través del deporte.`,
+        second_experience_task_4: `Dediqué tiempo a asistir a los miembros del equipo con una pequeña guía, ayudándolos a familiarizarse con herramientas de creación de contenido digital como Canva, asegurando que pudieran utilizarlas con confianza para mejorar su productividad y creatividad.`,
+        contact_section_title: `HÁZLO POSIBLE, EL 'IM' NO TIENE LUGAR SI LO DECIDES.`,
+        contact_section_subtitle: `Estoy trabajando para cumplir mis sueños y me gustaría colaborar con otros que también sueñen en grande. ¡Contáctame en LinkedIn o por correo!`,
+    }
+}
+
+function changeLanguage(lang) {
+    document.querySelectorAll('[data-key]').forEach(element => {
+        const key = element.getAttribute('data-key');
+        element.textContent = translations[lang][key];
+    });
+
+    document.querySelectorAll('.language a').forEach(el => el.classList.remove('active-lang'));
+    document.querySelector(`.language a[href="${lang.toUpperCase()}"]`).classList.add('active-lang');
+}
+
+document.querySelector('.language a[href="EN"]').addEventListener('click', (e) => {
+    e.preventDefault();
+    changeLanguage('en');
+});
+
+document.querySelector('.language a[href="ES"]').addEventListener('click', (e) => {
+    e.preventDefault();
+    changeLanguage('es');
+});
+
+changeLanguage('en');
+
 // Change the header style at scrolling
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
